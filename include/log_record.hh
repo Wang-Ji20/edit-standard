60bed8122f867a7fa0f3ec5cacb336200906cdc6
王集 <wangji05@kuaishou.com>
2023-08-16 14:57:42 +0800

    initial commit

---
+++ include/log_record.hh
@@ -0,0 +1,12 @@
+//===------------------------------------------===
+// estd 2023
+//
+// Identification: include/log_record.hh
+//
+// Author: Ji Wang <jiwangcdi@gmail.com>
+//
+//===------------------------------------------===
+
+struct LogRecord {
+
+};
---
+++ lib/log_record.cc
@@ -0,0 +1,11 @@
+//===------------------------------------------===
+// estd 2023
+//
+// Identification: lib/log_record.cc
+//
+// Author: Ji Wang <jiwangcdi@gmail.com>
+//
+//===------------------------------------------===
+
+#include "log_record.hh"
+
